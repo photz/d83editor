3cc2816c9c06f0f7373098df7c80a53eb6a65a52
Peter Beischmidt <peterbeischmidt@gmail.com>
2015-08-26 15:59:11 +0200

    it's now possible to open d83 files and display their contents

--- d83parser.js
+++ d83parser.js
@@ -22,10 +22,20 @@ var Quantity = function(pre, post, unit) {
 var d83toplevelnode = function(lvl1) {
     var that = this;
     
+    if (isNaN(lvl1)) {
+	throw new TypeError('the lvl1 of a top-level node may not be NaN');
+    }
+
+
     this.lvl1 = lvl1;
 
-    this.summary = summary;
+    this.nodeSummary = '';
 
+    this.summary = '';
+
+    this.nodes = {
+	watch: undefined
+    };
 
     this.toString = function() {
 	return that.summary;
@@ -35,19 +45,44 @@ var d83toplevelnode = function(lvl1) {
 var d83node = function(lvl1, lvl2) {
     var that = this;
 
+    if (isNaN(lvl1)) {
+	throw new TypeError('the lvl1 of a node may not be NaN');
+    }
+
+    if (isNaN(lvl2)) {
+	throw new TypeError('the lvl2 of a node may not be NaN');
+    }
+
+
     this.lvl1 = lvl1;
     this.lvl2 = lvl2;
 
-    this.summary = null;
+    this.nodeSummary = '';
+    this.summary = '';
 
+    this.entries = {
+	watch: undefined
+    };
 
     this.toString = function() {
-	return that.summary;
+	return that.lvl1.toString() + '/' + that.lvl2.toString() + ' ' + that.summary;
     };
 };
 
 var d83entry = function(lvl1, lvl2, lvl3) {
     var that = this;
+
+    if (isNaN(lvl1)) {
+	throw new TypeError('the lvl1 of an entry may not be NaN');
+    }
+
+    if (isNaN(lvl2)) {
+	throw new TypeError('the lvl2 of an entry may not be NaN');
+    }
+
+    if (isNaN(lvl3)) {
+	throw new TypeError('the lvl3 of an entry may not be NaN');
+    }
 
     this.lvl1 = lvl1;
     this.lvl2 = lvl2;
@@ -76,41 +111,93 @@ var d83parser = function(inputtext) {
     var currency = '';
 
     var currentEntry = null;
+    var currentNode = null;
+    var currentTopLevelNode = null;
     
+
     this.issuingCompany = '';
 
     this.entries = [];
 
     this.currency = '';
 
+    this.dir = {
+	watch: undefined
+    };
+
+    var commitCurrentNode = function() {
+	if (currentNode == null) {
+	    throw new Error();
+	}
+
+	if (currentTopLevelNode == null) {
+	    throw new Error('while trying to commit node ' +
+			    currentNode + ', currentTopLevelNode ' +
+			    'was null');
+	}
+
+	currentTopLevelNode.nodes[currentNode.lvl2] = currentNode;
+	currentNode = null;
+    };
+
+    var commitTopLevelNode = function() {
+	if (currentTopLevelNode == null) {
+	    throw new Error('trying to commit a top-level node, ' +
+			    'but currentTopLevelNode is null');
+	}
+
+	if (currentNode != null) {
+	    throw new Error();
+	}
+
+	if (currentEntry != null) {
+	    throw new Error();
+	}
+
+	if (currentTopLevelNode.lvl1 in that.dir) {
+	    throw new Error();
+	}
+
+	that.dir[currentTopLevelNode.lvl1] = currentTopLevelNode;
+	currentTopLevelNode = null;
+    };
+
     var commitEntry = function(line) {
 	if (currentEntry == null) {
-	    //console.log('trying to commit an entry, but there is no current entry');
-
-	    // throw {
-	    // 	name: 'NoEntryToCommit'
-	    // }
+	    throw new Error('trying to commit an entry, ' +
+			    'but currentEntry is null');
 	}
-	else {
-	    that.entries.push(currentEntry);
 
-	    //console.log('committing entry: ' + currentEntry);
-
-
-	    currentEntry = null;
+	if (!(currentEntry instanceof d83entry)) {
+	    throw new Error('trying to commit the current entry, ' +
+			    'but it is not a d83entry');
 	}
+
+	if (currentNode == null) {
+	    throw new Error('trying to commit an entry, but ' +
+			    'currentNode is null');
+	}
+
+	if (currentEntry.lvl3 in currentNode) {
+	    throw new Error('trying to commit an entry, but ' +
+			    'currentNode already has an entry with the same position');
+	}
+
+	currentNode.entries[currentEntry.lvl3] = currentEntry;
+	currentEntry = null;
     };
 
     
     var parseIssuingCompany = function(line) {
 	that.issuingCompany = line;
-
     };
 
     var parseCurrency = function(line) {
 	that.currency = line;
     };
 
+    
+    
     var parseNodeHeader = function(line) {
 	// if (currentEntry != null) {
 	//     console.log('found a node header, but a current entry exists');
@@ -127,15 +214,28 @@ var d83parser = function(inputtext) {
 	var lvl1 = parseInt(line.substring(0, 2));
 	var lvl2 = parseInt(line.substring(2, 4));
 	
-	if (lvl2 == NaN) {
+	if (isNaN(lvl2)) {
 	    // lvl 1 node
 	    
-	    currentEntry = new d83toplevelnode(lvl1);
+	    if (currentNode != null) {
+		commitCurrentNode();
+	    }
+
+	    if (currentTopLevelNode != null) {
+		commitTopLevelNode();
+	    }
+
+	    currentTopLevelNode = new d83toplevelnode(lvl1);
 	}
-	else {
+	else {// lvl2 != Nan
 	    // lvl 2 node
 
-	    currentEntry = new d83node(lvl1, lvl2);
+	    if (currentNode != null) {
+		commitCurrentNode();
+	    }
+
+
+	    currentNode = new d83node(lvl1, lvl2);
 	}
     };
 
@@ -171,39 +271,50 @@ var d83parser = function(inputtext) {
     };
 
     var parseNodeSummary = function(line) {
-	if (currentEntry == null) {
 
+	if (currentNode != null) {
+	    currentNode.nodeSummary += line.trim() + ' ';
 	}
-
-	currentEntry.nodeSummary += line.trim() + ' '
+	else if (currentTopLevelNode != null) {
+	    currentTopLevelNode.nodeSummary += line.trim() + ' ';
+	}
+	else {
+	    throw new Error('found a node summary, but there are no ' +
+			    'current nodes or entries');
+	}
     };
 
     var parseSummary = function(line) {
-	if (currentEntry == null) {
-	    //console.log('found a summary, but no current entry exists');
 
-	    // throw {
-	    // 	name: 'NoCurrentEntry'
-	    // }
+	if (currentEntry != null) {
+	    currentEntry.summary += line.trim() + ' ';
+	}
+	else if (currentNode != null) {
+	    currentNode.summary += line.trim() + ' ';
+	}
+	else if (currentTopLevelNode != null) {
+	    currentTopLevelNode.summary += line.trim() + ' ';
 	}
 	else {
-	    currentEntry.summary += line.trim() + ' ';
+	    throw new Error('found a node summary, but there are no ' +
+			    'current nodes or entries');
 	}
     };
 
     var parseDescription = function(line) {
-	//console.log('found desc: ' + line);
-
-	if (currentEntry == null) {
-	    //console.log('found a description, but no current entry exists');
+	if (currentEntry != null) {
+	    currentEntry.description += line.trim() + ' ';
+	}
+	else if (currentNode != null) {
+	    currentNode.description += line.trim() + ' ';
+	}
+	else if (currentTopLevelNode != null) {
+	    currentTopLevelNode.description += line.trim() + ' ';
 	}
 	else {
-	    currentEntry.description += line.trim() + ' '
-
-
+	    throw new Error('found a node summary, but there are no ' +
+			    'current nodes or entries');
 	}
-
-
     };
 
 
