5f82d9fb6aad5748a8f2d1856741f08cb5d6ca92
Peter Beischmidt <peterbeischmidt@gmail.com>
2015-08-27 18:59:24 +0200

    cleaning up the source

--- EntryView.js
+++ EntryView.js
@@ -2,13 +2,102 @@
 var EntryView = function(entry) {
     var that = this;
 
-    var row = document.createElement('div');
-    row.classList.add('row');
-    row.classList.add('list-group-item');
-    row.style.cursor = 'pointer';
+    if (!(entry instanceof d83entry)) {
+	throw new TypeError('expecting a d83entry');
+    }
+    
+    var box = null;
+
+    var externalUserChangePriceCallback;
+
+    
+    (function() {
+
+	//
+	// setup for the containing div element
+	//
+
+	box = document.createElement('div');
+	box.classList.add('row');
+	box.classList.add('list-group-item');
+	box.style.cursor = 'pointer';
+
+	//
+	// setup for the position/path column
+	//
+
+	var posField = document.createElement('div');
+	posField.classList.add('col-md-1');
+	box.appendChild(posField);
+
+	//
+	// create a label for this entry's path/position
+	//
+	var label = document.createElement('span');
+	label.classList.add('label');
+	label.classList.add('label-default');
+	label.appendChild(document.createTextNode(entry.getPrettyPath()));
+	posField.appendChild(label);
+
+	//
+	// setup for the summary column
+	//
+	var summaryField = document.createElement('div');
+	box.appendChild(summaryField);
+	summaryField.classList.add('col-md-6');
+	summaryField.innerHTML = entry.summary;
+
+	
+	//
+	// column for the quantity
+	//
+	var quantityField = document.createElement('div');
+	quantityField.classList.add('col-md-2');
+	quantityField.innerHTML = entry.quantity;
+	box.appendChild(quantityField);
+
+	//
+	// column for the price
+	//
+	var priceField = document.createElement('div');
+	priceField.classList.add('col-md-2');
+	priceField.classList.add('form-group');
+	box.appendChild(priceField);
+	
+	var priceInput = document.createElement('input');
+	priceInput.classList.add('form-control');
+	priceField.appendChild(priceInput);
+
+	//
+	// column for the total price
+	//
+	var totalPriceField = document.createElement('div');
+	totalPriceField.classList.add('col-md-1');
+	box.appendChild(totalPriceField);
+
+	//
+	// callback to update the total price
+	//
+	priceInput.addEventListener('change', function(evt) {
+	    if (isNaN(priceInput.value)) {
+		priceField.classList.add('has-error');
+	    }
+	    else {
+		entry.netPricePerUnit = parseFloat(priceInput.value);
+		priceField.classList.remove('has-error');
+		
+		totalPriceField.innerHTML =
+		    '<p>' + entry.getNetTotal() + '</p>';
+
+		externalUserChangePriceCallback();
+	    }
+	});
+
+    })();
+
 
     this.getElement = function() {
-	return row;
+	return box;
     };
 
     this.setClickCallback = function(func) {
@@ -16,53 +105,14 @@ var EntryView = function(entry) {
 	    throw new TypeError('expecting a function as a callback');
 	}
 
-	row.onclick = function(evt) {
+	box.onclick = function(evt) {
 	    func(that);
 	};
     };
 
-
-    var posField = document.createElement('div');
-    posField.classList.add('col-md-1');
-    row.appendChild(posField);
-
-    // create a label for this entry's path/position
-    var label = document.createElement('span');
-    label.classList.add('label');
-    label.classList.add('label-default');
-    label.appendChild(document.createTextNode(entry.getPrettyPath()));
-    posField.appendChild(label);
-
-    var summaryField = document.createElement('div');
-    row.appendChild(summaryField);
-    summaryField.classList.add('col-md-6');
-    summaryField.innerHTML = entry.summary;
-
     this.getEntry = function() {
 	return entry;
     };
-    
-    //
-    // column for the quantity
-    //
-    var quantityField = document.createElement('div');
-    quantityField.classList.add('col-md-2');
-    quantityField.innerHTML = entry.quantity;
-    row.appendChild(quantityField);
-
-    //
-    // column for the price
-    //
-    var priceField = document.createElement('div');
-    priceField.classList.add('col-md-2');
-    priceField.classList.add('form-group');
-    row.appendChild(priceField);
-    
-    var priceInput = document.createElement('input');
-    priceInput.classList.add('form-control');
-    priceField.appendChild(priceInput);
-
-    var externalUserChangePriceCallback;
 
     this.setUserChangePriceCallback = function(func) {
 	if (typeof(func) != 'function') {
@@ -72,37 +122,13 @@ var EntryView = function(entry) {
 	externalUserChangePriceCallback = func;
     };
     
-    //
-    // column for the total price
-    //
-    var totalPriceField = document.createElement('div');
-    totalPriceField.classList.add('col-md-1');
-    row.appendChild(totalPriceField);
-
-    //
-    // callback to update the total price
-    //
-    priceInput.addEventListener('change', function(evt) {
-	if (isNaN(priceInput.value)) {
-	    priceField.classList.add('has-error');
-	}
-	else {
-	    entry.netPricePerUnit = parseFloat(priceInput.value);
-	    priceField.classList.remove('has-error');
-	    
-	    totalPriceField.innerHTML =
-		'<p>' + entry.getNetTotal() + '</p>';
-
-	    externalUserChangePriceCallback();
-	}
-    });
 
     this.setActive = function() {
-	row.classList.add('active');
+	box.classList.add('active');
     };
 
     this.setInactive = function() {
-	row.classList.remove('active');
+	box.classList.remove('active');
     };
 
 };
